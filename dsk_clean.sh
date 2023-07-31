73bc06c85cc9e585ff32a9f0de50f7fac1ae02bc
R. Davis <42743080+rd636@users.noreply.github.com>
2023-07-31 12:24:21 -0500

    Update dsk_clean.sh

--- dsk_clean.sh
+++ dsk_clean.sh
@@ -4,7 +4,8 @@
 #    whatsoever, express, implied, written, oral or statutory, including 
 #    without limitation warranties of merchantability, fitness for a 
 #    particular purpose, title and noninfringement.
-#
+
+###
 ### Free space in /var
 ### IAW https://docs.netscaler.com/en-us/citrix-adc/current-release/system/troubleshooting-citrix-adc/how-to-free-space-on-var-directory.html
 ###
@@ -17,9 +18,11 @@ rm -rf "/var/crash"/*
 rm -rf "/var/nsinstall"/*
 rm -rf "/var/mastools/logs"/*
 rm -rf "/var/ns_system_backup"/*
+
 ###
 ### Free Space in the /flash IAW CTX133587
 ###
+
 # Define the current kernel in use
 current_kernel=$(cat /flash/boot/loader.conf | grep kernel | sed -nr -E 's/kernel=\"\/(.*)\"/\1/p')
 current_kernel="/flash/"$current_kernel".gz"
