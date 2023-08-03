320e76267bc90e7efdc6ab262710937047ac0528
R. Davis <42743080+rd636@users.noreply.github.com>
2023-08-03 16:42:11 -0500

    Update invalid.pl
    tests for and addresses encrypted files

--- invalid.pl
+++ invalid.pl
@@ -2,7 +2,7 @@
 # 
 #   This program warns when cert and key names are using invalid characters. 
 #   Only valid characters are A-Za-z0-9._-
-#   Corrective actions are included in the output.
+#   Corrective actions are inlcuded in the output.
 #
 #   Success = exit 0 - No invalid characters found.
 #   Failure = exit 1 - Files needing correction are listed in the cli output.
@@ -24,7 +24,6 @@
 # 1 Files found with invalid characters:
 # CNA Issuing CA-SHA2.cer
 #
-#
 
 use strict;
 use warnings;
@@ -45,25 +44,34 @@ sub get_file_names_from_file {
     while (my $line = <$fh>) {
         if ($line =~ /add ssl certKey (?|"([^"]*)"|(\S+)) -cert (?|"([^"]*)"|(\S+)) -key (?|"([^"]*)"|(\S+))($|\s)/) {
             push @file_names, $2, $3;
-            # If invalid characters are used in the certificate, provide corrective commands
-            if (invalid_test($2)) {
-            my $cert = invalid_test($2);
-                print "# cert: $2 \n";
-                print "shell cd /nsconfig/ssl && cp -n \"$2\" $cert \n";
-                print "update ssl certKey \"$1\" -cert $cert \n# \n";
+            my $cert = $2;
+            my $key = $3;
+            # If invalid characters are used, provide corrective commands
+            if ( invalid_test($cert) || invalid_test($key) ) {
+                # my $cert = invalid_test($2);
+                print "# certkey: $1 c:$cert k:$key\n";
+                if ( invalid_test($cert) ) { 
+                    # certificate name needs changing
+                    $cert = change_filename($cert);
+                    print "shell cd /nsconfig/ssl && cp -n \"$2\" $cert \n";
+                }
+                if ( invalid_test($key) ) {
+                    # keyname needs changing
+                    $key = change_filename($key);
+                    print "shell cd /nsconfig/ssl && cp -n \"$3\" $key \n";
+                }
+                # update ssl certKey <certkeyName> [-cert <string> [-password]] [-key <string> 
+                if ( encryption_test($3) ) {
+                    print "update ssl certKey \"$1\" -cert $cert -key $key -password #### \n# \n";
+                } else {
+                    print "update ssl certKey \"$1\" -cert $cert -key $key \n# \n";
+                }
             }
-            # If invalid characters are used in the key, provide corrective commands
-            if (invalid_test($3)) {
-                my $key = invalid_test($3);
-                print "# key $3 \n";
-                print "shell cd /nsconfig/ssl && cp -n \"$3\" $key \n";
-                print "update ssl certKey \"$1\" -key $key \n# \n";
-            }          
         } elsif ($line =~ /add ssl certKey (?|"([^"]*)"|(\S+)) -cert (?|"([^"]*)"|(\S+))($|\s)/g) {
             push @file_names, $2;
             # If invalid characters are used in the CA Certificate, provide corrective commands
             if (invalid_test($2)) {
-                my $cert = invalid_test($2);
+                my $cert = change_filename($2);
                 print "# ca cert: $2 \n";
                 print "shell cd /nsconfig/ssl && cp -n \"$2\" $cert \n";
                 print "update ssl certKey \"$1\" -cert $cert \n# \n";
@@ -74,16 +82,29 @@ sub get_file_names_from_file {
     return @file_names;
 }
 
-sub invalid_test {
+sub encryption_test {
+    my ($filename) = @_;
+    my $encryption = `cd /nsconfig/ssl/ && grep -c ENCRYPTED $filename`;
+    chomp $encryption;
+    if ($encryption eq "1") {
+        # file has encryption  
+        return(1);
+    } else { 
+        # file does not has encryption  
+        return(0);
+    }
+}
+
+sub change_filename {
     my ($filename) = @_;
     if ($filename =~ /[^A-Za-z0-9._-]/) {
         # Strip file location information
         if ($filename =~ /\//) {
             ($filename) = $filename =~ m#([^/]+)$#;
-            # To prevent a conflict, check if a file of the same name already exists
+            # check if a file of the same name already exists and warn 
             if (-e '/nsconfig/ssl/'.$filename) {
                 # file already exists in 
-                print "# \n# ERROR: /nsconfig/ssl/$filename already exists \n# \n"
+                print "# \n# WARNING: /nsconfig/ssl/$filename already exists \n# \n"
             }
         }
         # Strip remaining invalid characters 
@@ -91,6 +112,15 @@ sub invalid_test {
         return($filename);
     } else {
         # Valid characters in use
+        return(0);
+    }
+}
+
+sub invalid_test {
+    my ($filename) = @_;
+    if ($filename =~ /[^A-Za-z0-9._-]/) {
+        return(1);
+    } else {
         return(0);
     }
 }
